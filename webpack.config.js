742bdbaa9c74929597bad9cdafb747cf92fa279d
mdlepley <mdlepley@gmail.com>
2017-02-25 23:01:29 -0600

    Add standard packages
    Add React, webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
